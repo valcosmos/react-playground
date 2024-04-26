a39de08e493cf1c63e725de1537484ac0913fc8d
Cupid Valentine <westerngod@126.com>
2024-04-26 23:52:33 +0800

    feat: setup header

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import logoSvg from '../icons/logo.svg'
+
+export default function Header() {
+  return (
+    <header className="h-12 px-5 border-b border-black flex items-center justify-between">
+      <div className="text-xl flex items-center">
+        <Image alt="logo" className='h-6 mr-2' src={logoSvg} />
+        <span>React Playground</span>
+      </div>
+    </header>
+  )
+}
--- components/ReactPlayground.tsx
+++ components/ReactPlayground.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client'
+import { Allotment } from 'allotment'
+import 'allotment/dist/style.css'
+import Header from './Header'
+import CodeEditor from './CodeEditor'
+import Preview from './Preview'
 
 export default function ReactPlayground() {
-  return (
-    <div>ReactPlayground</div>
-  )
+ return (
+   <div className='h-screen'>
+     <Header />
+     <Allotment defaultSizes={[100, 100]}>
+       <Allotment.Pane minSize={500}>
+         <CodeEditor />
+       </Allotment.Pane>
+       <Allotment.Pane minSize={0}>
+         <Preview />
+       </Allotment.Pane>
+     </Allotment>
+   </div>
+ )
 }
